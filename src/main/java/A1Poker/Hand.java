0150aa3fd3e256b34a0f298dcda54e195f709277
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-09-13 18:16:51 -0400

    Added second test that mirrors the first one and begining code
    Started the code to add the cards to the players hands, added the tests to see if there are 5 cards in their hands

--- src/main/java/A1Poker/Hand.java
+++ src/main/java/A1Poker/Hand.java
@@ -1,5 +1,10 @@
 package A1Poker;
 
-public class Hand {
+import java.io.File;
 
+public class Hand {
+	String hand[] = new String[5];
+	File file = new File(".\\src\\main\\resources\\A1Poker\\cards.txt");
+	
+	
 }
--- src/test/java/A1Poker/HandTest.java
+++ src/test/java/A1Poker/HandTest.java
@@ -1,7 +1,5 @@
 package A1Poker;
 
-import java.io.File;
-
 import junit.framework.TestCase;
 
 public class HandTest extends TestCase {
@@ -9,9 +7,9 @@ public class HandTest extends TestCase {
 	public void AddTest() {
 		Hand handAIP = new Hand();
 		Hand handP2 = new Hand();
-		File file = new File(".\\src\\main\\resources\\A1Poker\\cards.txt");
 		
-		assertEquals(5, handP2.hand.length());
+		assertEquals(5, handP2.hand.length);
+		assertEquals(5, handAIP.hand.length);
 	}
 
 }
