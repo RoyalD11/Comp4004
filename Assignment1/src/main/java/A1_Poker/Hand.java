d8ee93bf452fbbf0d04779760398e72eff25c931
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-09-26 15:37:02 -0400

    All Hand Tests Pass
    Got adding cards to hand, opening the file and closing the file all to pass. Now to refactor so I have a Deck Class to draw cards from

--- Assignment1/src/main/java/A1_Poker/Hand.java
+++ Assignment1/src/main/java/A1_Poker/Hand.java
@@ -4,28 +4,41 @@ import java.io.*;
 import java.util.*;
 
 public class Hand {
+	
+	//Global variables to be used by all functions in this class. 
 	ArrayList <String> hand = new ArrayList<String>();
 	Scanner sc;
 	
+	//Function that will draw cards from a text file into the players hands
 	public int drawHand() {
 		
 		for(int i = 0; i<5; i++) {
 			hand.add(i, sc.nextLine());
 		}
+		
+		for(int i = 0; i<hand.size(); i++) {
+			System.out.println(hand.get(i));
+		}
 
 		return hand.size();
 	}
 	
-	public void openFile() {
+	//Function that will open the text file, tries to open it if it fails it prints the error.
+	public boolean openFile(String fileName) {
 		try {
-			sc = new Scanner(new File("src/main/resources/cards.txt"));
+			//Sets the scanner to be the location of the text file.
+			sc = new Scanner(new File("src/main/resources/" + fileName));
+			return true;
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
+			return false;
 		}
 	}
 	
-	public void closeFile() {
+	//Function that will close the text file
+	public boolean closeFile() {
 		sc.close();
+		return true;
 	}
 }
--- Assignment1/src/test/java/A1_Poker/handTest.java
+++ Assignment1/src/test/java/A1_Poker/handTest.java
@@ -4,14 +4,42 @@ import junit.framework.TestCase;
 
 public class handTest extends TestCase {
 	
+	public void testOpenFile() {
+		//Creates a player object from the hand class
+		Hand playerAIP = new Hand();
+		Hand playerP2 = new Hand();
+
+		
+		//Tests if the file is actually opened.
+		assertEquals(true, playerAIP.openFile("cards.txt"));
+		assertEquals(true, playerP2.openFile("cards.txt"));
+
+	}
+	
+	public void testCloseFile() {
+		//Creates a player object from the hand class
+		Hand playerAIP = new Hand();
+		Hand playerP2 = new Hand();
+
+		playerAIP.openFile("cards.txt");
+		playerP2.openFile("cards.txt");
+		
+		//Tests if the file is actually opened.
+		assertEquals(true, playerAIP.closeFile());
+		assertEquals(true, playerP2.closeFile());
+
+	}
+	
 	public void testHandSize() {
 		Hand playerAIP = new Hand();
+		playerAIP.openFile("cards.txt");
 		
 		assertEquals(5, playerAIP.drawHand());
 	}
 	
 	public void testHandSizeP2() {
 		Hand playerP2 = new Hand();
+		playerP2.openFile("cards.txt");
 		
 		assertEquals(5, playerP2.drawHand());
 	}
