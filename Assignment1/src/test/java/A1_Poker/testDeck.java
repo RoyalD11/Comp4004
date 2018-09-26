fcf8a2db8519109647421898ce75123f71879a82
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-09-26 15:46:22 -0400

    Created Deck class with one test
    Currently Fails, will eventually open the file that holds all the cards

--- Assignment1/src/test/java/A1_Poker/testDeck.java
+++ Assignment1/src/test/java/A1_Poker/testDeck.java
@@ -1,5 +1,16 @@
 package A1_Poker;
 
-public class testDeck {
+import junit.framework.TestCase;
 
+public class testDeck extends TestCase{
+
+	public void testOpenFile() {
+		//Creates a player object from the hand class
+		Deck allCards = new Deck();
+
+		
+		//Tests if the file is actually opened.
+		assertEquals(true, allCards.openFile("cards.txt"));
+
+	}
 }
