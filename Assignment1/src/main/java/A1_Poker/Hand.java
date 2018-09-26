08b36e1218cdfe0952b9bcd4e77785539f02e416
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-09-26 16:18:19 -0400

    Cards are added to hand
    Code has been refactored to include a deck class, Cards are drawn from the deck class into the array list stored in the hand class.

--- Assignment1/src/main/java/A1_Poker/Hand.java
+++ Assignment1/src/main/java/A1_Poker/Hand.java
@@ -8,10 +8,13 @@ public class Hand {
 	//Global variables to be used by all functions in this class. 
 	ArrayList <String> hand = new ArrayList<String>();
 	Deck allCards = new Deck();
+	
 
 	
 	//Function that will draw cards from a text file into the players hands
 	public int drawHand() {
+		allCards.openFile("cards.txt");
+
 		
 		for(int i = 0; i<5; i++) {
 			hand.add(i, allCards.sc.nextLine());
--- Assignment1/src/test/java/A1_Poker/handTest.java
+++ Assignment1/src/test/java/A1_Poker/handTest.java
@@ -6,13 +6,9 @@ public class handTest extends TestCase {
 	
 	public void testHandSize() {
 		Hand playerAIP = new Hand();
-		
-		assertEquals(5, playerAIP.drawHand());
-	}
-	
-	public void testHandSizeP2() {
 		Hand playerP2 = new Hand();
-		
+
+		assertEquals(5, playerAIP.drawHand());
 		assertEquals(5, playerP2.drawHand());
 	}
 }
