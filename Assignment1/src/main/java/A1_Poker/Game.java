d640ea87c824f8953c05b87d73334ec7c485882e
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-10-04 23:53:34 -0400

    Logic to test for Flush
    Wrote logic to test if the hand has a flush, will return true if there is one and false by default

--- Assignment1/src/main/java/A1_Poker/Game.java
+++ Assignment1/src/main/java/A1_Poker/Game.java
@@ -1,5 +1,24 @@
 package A1_Poker;
 
+import java.util.ArrayList;
+
 public class Game {
 
+	public boolean flush(ArrayList<String> hand) {
+		
+		int counter = 0;
+		
+		for(int i=1; i<hand.size(); i++) {
+			if(hand.get(i-1).substring(0, 1) != hand.get(i).substring(0, 1)) {
+				break;
+			}
+			if(hand.get(i-1).substring(0, 1) == hand.get(i).substring(0, 1)) {
+				counter++;
+			}
+			if(counter == 4) return true;
+		}
+		
+		
+		return false;
+	}
 }
