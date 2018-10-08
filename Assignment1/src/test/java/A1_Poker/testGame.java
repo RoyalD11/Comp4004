f2182d819d8a8f5230559376fc62df4cc8396458
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-10-08 17:02:29 -0400

    TestFullHouse
    test method that will be for a full house

--- Assignment1/src/test/java/A1_Poker/testGame.java
+++ Assignment1/src/test/java/A1_Poker/testGame.java
@@ -3,139 +3,150 @@ package A1_Poker;
 import junit.framework.TestCase;
 
 public class testGame extends TestCase {
-	
+
 	public void testRoyalFlush() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("royalFlush.txt");
-		
+
 		String cards[] = deck.drawFive();
-		
-		for (int i = 0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
 		assertEquals(true, game.royalFlush(playerAIP));
 	}
-	
+
 	public void testStrightFlush() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("straightFlush.txt");
-		
+
 		String cards[] = deck.drawFive();
-		
-		for (int i = 0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
 		assertEquals(true, game.straightFlush(playerAIP));
 	}
-	
+
 	public void testFourOfAKind() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("fourOAC.txt");
-		
-		String cards[] = deck.drawFive();
-		
-		for (int i = 0; i<5; i++) {
-			playerAIP.hand.add(cards[i]);
-		}
-		
-		assertEquals(true, game.duplicateRank(playerAIP.hand));
-	}
-	
-	public void testFlush() {
-		Hand  playerAIP = new Hand();
-		Deck deck = new Deck();
-		Game game = new Game();
-		
-		deck.openFile("flush.txt");
-		
+
 		String cards[] = deck.drawFive();
 
-		
-		for (int i = 0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
+		assertEquals(true, game.duplicateRank(playerAIP.hand));
+	}
+
+	public void testFullHouse() {
+		Hand playerAIP = new Hand();
+		Deck deck = new Deck();
+		Game game = new Game();
+
+		deck.openFile("fullHouse.txt");
+
+		String cards[] = deck.drawFive();
+
+		for (int i = 0; i < 5; i++) {
+			playerAIP.hand.add(cards[i]);
+		}
+
+		assertEquals(true, game.fullHouse(playerAIP.hand));
+	}
+
+	public void testFlush() {
+		Hand playerAIP = new Hand();
+		Deck deck = new Deck();
+		Game game = new Game();
+
+		deck.openFile("flush.txt");
+
+		String cards[] = deck.drawFive();
+
+		for (int i = 0; i < 5; i++) {
+			playerAIP.hand.add(cards[i]);
+		}
+
 		assertEquals(true, game.flush(playerAIP.hand));
 	}
-	
+
 	public void testStraight() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("straight.txt");
-		
+
 		String cards[] = deck.drawFive();
 
-		
-		for (int i = 0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
 		assertEquals(true, game.straight(playerAIP));
 	}
-	
+
 	public void testThreeOfAKind() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("threeOAC.txt");
-		
+
 		String cards[] = deck.drawFive();
-		
-		for (int i = 0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
 		assertEquals(true, game.duplicateRank(playerAIP.hand));
 
-		
 	}
-	
+
 	public void testPair() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("pair.txt");
-		
+
 		String cards[] = deck.drawFive();
-		
-		for (int i = 0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
 		assertEquals(true, game.duplicateRank(playerAIP.hand));
 
-		
 	}
 
 	public void testHighCard() {
-		Hand  playerAIP = new Hand();
+		Hand playerAIP = new Hand();
 		Deck deck = new Deck();
 		Game game = new Game();
-		
+
 		deck.openFile("highCard.txt");
-		
+
 		String cards[] = deck.drawFive();
 
-		
-		for (int i = 0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			playerAIP.hand.add(cards[i]);
 		}
-		
+
 		assertEquals("SA", game.highCard(playerAIP));
 	}
 }
