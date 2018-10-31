e4825e30a190cedcc0d215f109fca08abb744bb0
Andrew Dodge <33609339+RoyalD11@users.noreply.github.com>
2018-10-31 16:01:01 -0400

    next parts of grid

---
+++ Assignment2/Features/HTBWins.feature
@@ -0,0 +1,103 @@
+Feature: HTB Wins
+
+# ROYAL FLUSH
+Scenario: HTB has royal flush beats AIP straight flush
+		Given HTB has a royal flush and AIP has a straight flush "H10 HJ HQ HK HA D4 D5 D6 D7 D8"
+		Then HTB wins 
+		
+Scenario: HTB has royal flush beats AIP 4 of a kind
+		Given HTB has a royal flush and AIP has a four of a kind "H10 HJ HQ HK HA D4 S4 H4 C4 D8"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP full house
+		Given HTB has royal flush beats AIP full house "H10 HJ HQ HK HA D4 S8 H4 C4 D8"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP flush
+		Given HTB has royal flush beats AIP flush "H10 HJ HQ HK HA D10 D8 D4 D7 D2"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP straight
+		Given HTB has royal flush beats AIP straight "H10 HJ HQ HK HA D4 S5 H6 C7 D8"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP 3 of a kind
+		Given HTB has royal flush beats AIP three of a kind "H10 HJ HQ HK HA D7 S4 H4 C4 D8"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP two pairs
+		Given HTB has royal flush beats AIP two pairs "H10 HJ HQ HK HA D4 S8 H3 C4 D8"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP one pair
+		Given HTB has royal flush beats AIP one pair "H10 HJ HQ HK HA D6 S4 H4 C10 D8"
+		Then HTB wins
+		
+Scenario: HTB has royal flush beats AIP high card
+		Given HTB has royal flush beats AIP high card "H10 HJ HQ HK HA D9 SJ H4 C6 D8"
+		Then HTB wins
+
+
+# STRAIGHT FLUSH
+Scenario: HTB has straight flush beats AIP 4 of a kind
+		Given HTB has a straight flush and AIP has a four of a kind "D4 D5 D6 D7 D8 S10 D10 H10 C10 SJ"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP full house
+		Given HTB has straight flush beats AIP full house "D4 D5 D6 D7 D8 C8 S8 H4 C4 H8"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP flush
+		Given HTB has straight flush beats AIP flush "D4 D5 D6 D7 D8 H10 H8 H4 H7 H2"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP straight
+		Given HTB has straight flush beats AIP straight "D4 D5 D6 D7 D8 C4 S5 H6 C7 C8"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP 3 of a kind
+		Given HTB has straight flush beats AIP three of a kind "D4 D5 D6 D7 D8 C7 S4 H4 C4 C8"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP two pairs
+		Given HTB has straight flush beats AIP two pairs "D4 D5 D6 D7 D8 S4 S8 H3 C4 H8"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP one pair
+		Given HTB has straight flush beats AIP one pair "D4 D5 D6 D7 D8 S6 S4 H4 C10 H8"
+		Then HTB wins
+		
+Scenario: HTB has straight flush beats AIP high card
+		Given HTB has straight flush beats AIP high card "D4 D5 D6 D7 D8 H9 SJ H4 C6 H8"
+		Then HTB wins
+		
+		
+# FOUR OF A KIND
+Scenario: HTB has four of a kind beats AIP full house
+		Given HTB has four of a kind beats AIP full house "S10 D10 H10 C10 SJ C8 S8 H4 C4 H8"
+		Then HTB wins
+		
+Scenario: HTB has four of a kind beats AIP flush
+		Given HTB has four of a kind beats AIP flush "S10 D10 H10 C10 SJ HJ H8 H4 H7 H2"
+		Then HTB wins
+		
+Scenario: HTB has four of a kind beats AIP straight
+		Given HTB has four of a kind beats AIP straight "S10 D10 H10 C10 SJ C4 S5 H6 C7 C8"
+		Then HTB wins
+		
+Scenario: HTB has four of a kind beats AIP 3 of a kind
+		Given HTB has four of a kind beats AIP three of a kind "S10 D10 H10 C10 SJ C7 S4 H4 C4 C8"
+		Then HTB wins
+		
+Scenario: HTB has four of a kind beats AIP two pairs
+		Given HTB has four of a kind beats AIP two pairs "S10 D10 H10 C10 SJ S4 S8 H3 C4 H8"
+		Then HTB wins
+		
+Scenario: HTB has four of a kind beats AIP one pair
+		Given HTB has four of a kind beats AIP one pair "S10 D10 H10 C10 SJ S6 S4 H4 C10 H8"
+		Then HTB wins
+		
+Scenario: HTB has four of a kind beats AIP high card
+		Given HTB has four of a kind beats AIP high card "S10 D10 H10 C10 SJ H9 CJ H4 C6 H8"
+		Then HTB wins
+		
---
+++ Assignment2/src/main/java/TestCode/HTBWinsDefinitions.java
@@ -0,0 +1,309 @@
+package TestCode;
+
+import org.junit.Assert;
+
+import core.Game;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class HTBWinsDefinitions {
+
+	Game game;
+
+	//ROYAL FLUSH
+	@Given("^HTB has a royal flush and AIP has a straight flush \"([^\"]*)\"$")
+	public void HTB_has_a_royal_flush_and_AIP_has_a_straight_flush(String deck) {
+
+		System.out.println("\n------ROYAL FLUSH-------");
+		System.out.println("------HTB Has Royal Flush vs AIP Straight Flush-------");
+
+		// Make a new game object
+		game = new Game();
+
+		// Evaluate the hands given to see which hand is better from the 10 cards
+		game.evaluate(deck);
+
+		// Find the winner based of the evaluation done above
+		game.findWinner();
+
+	}
+
+	@Given("^HTB has a royal flush and AIP has a four of a kind \"([^\"]*)\"$")
+	public void htb_has_a_royal_flush_and_AIP_has_a_four_of_a_kind(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Four of a Kind-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+
+	}
+
+	@Given("^HTB has royal flush beats AIP full house \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_full_house(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Full House-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+
+	}
+
+	@Given("^HTB has royal flush beats AIP flush \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_flush(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Flush-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has royal flush beats AIP straight \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_straight(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Straight-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has royal flush beats AIP three of a kind \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_three_of_a_kind(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Three of a Kind-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has royal flush beats AIP two pairs \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_two_pairs(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Two Pair-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has royal flush beats AIP one pair \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_one_pair(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP Pair-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has royal flush beats AIP high card \"([^\"]*)\"$")
+	public void htb_has_royal_flush_beats_AIP_high_card(String deck) {
+		System.out.println("\n------HTB Has Royal Flush vs AIP High Card-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+	
+	
+	//STRAIGHT FLUSH
+	@Given("^HTB has a straight flush and AIP has a four of a kind \"([^\"]*)\"$")
+	public void htb_has_a_straight_flush_and_AIP_has_a_four_of_a_kind(String deck) {
+		System.out.println("\n------STRAIGHT FLUSH-------");
+		System.out.println("------HTB Has Straight Flush vs AIP Four of a Kind-------");
+
+		// Make a new game object
+		game = new Game();
+
+		// Evaluate the hands given to see which hand is better from the 10 cards
+		game.evaluate(deck);
+
+		// Find the winner based of the evaluation done above
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP full house \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_full_house(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP Full House-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP flush \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_flush(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP Flush-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP straight \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_straight(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP Straight-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP three of a kind \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_three_of_a_kind(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP Three of a Kind-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP two pairs \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_two_pairs(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP Two Pair-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP one pair \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_one_pair(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP Pair-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has straight flush beats AIP high card \"([^\"]*)\"$")
+	public void htb_has_straight_flush_beats_AIP_high_card(String deck) {
+		System.out.println("------HTB Has Straight Flush vs AIP High Card-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+	
+	
+	
+	//FOUR OF A KIND
+	@Given("^HTB has four of a kind beats AIP full house \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_full_house(String deck) {
+		System.out.println("\n------FOUR OF A KIND-------");
+		System.out.println("------HTB Has Four of a Kind vs AIP Full House-------");
+
+		// Make a new game object
+		game = new Game();
+
+		// Evaluate the hands given to see which hand is better from the 10 cards
+		game.evaluate(deck);
+
+		// Find the winner based of the evaluation done above
+		game.findWinner();
+	}
+
+	@Given("^HTB has four of a kind beats AIP flush \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_flush(String deck) {
+		System.out.println("------HTB Has Four of a Kind vs AIP Flush-------");
+		
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has four of a kind beats AIP straight \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_straight(String deck) {
+		System.out.println("------HTB Has Four of a Kind vs AIP Straight-------");
+		
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has four of a kind beats AIP three of a kind \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_three_of_a_kind(String deck) {
+		System.out.println("------HTB Has Four of a Kind vs AIP Three of a Kind-------");
+		
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has four of a kind beats AIP two pairs \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_two_pairs(String deck) {
+		System.out.println("------HTB Has Four of a Kind vs AIP Two Pair-------");
+		
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has four of a kind beats AIP one pair \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_one_pair(String deck) {
+		System.out.println("------HTB Has Four of a Kind vs AIP Pair-------");
+		
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Given("^HTB has four of a kind beats AIP high card \"([^\"]*)\"$")
+	public void htb_has_four_of_a_kind_beats_AIP_high_card(String deck) {
+		System.out.println("------HTB Has Four of a Kind vs AIP High Card-------");
+
+		game = new Game();
+
+		game.evaluate(deck);
+
+		game.findWinner();
+	}
+
+	@Then("^HTB wins$")
+	public void HTB_wins() {
+
+		Assert.assertEquals(game.AIPWon(), false);
+
+		System.out.println("HTB Hand: " + game.toBeat.cards);
+		System.out.println("AIP Hand: " + game.AIP.cards);
+		System.out.println("HTB beat AIP\n\n");
+	}
+	
+}
